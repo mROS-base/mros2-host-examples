51b54f95b9f65f35a5a33fbd32d734751290c36a
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2021-08-18 17:29:12 +0900

    formatten by astyle -n --style=1tbs -s2

--- mros2_echoback/src/pub_node.cpp
+++ mros2_echoback/src/pub_node.cpp
@@ -13,31 +13,31 @@ using namespace std::chrono_literals;
 
 class Publisher : public rclcpp::Node
 {
-  public:
-    Publisher()
+public:
+  Publisher()
     : Node("pub_mros2"), count_(0)
-    {
-      publisher_ = this->create_publisher<std_msgs::msg::String>("to_stm", 10);
-      timer_ = this->create_wall_timer(1000ms, std::bind(&Publisher::timer_callback, this));
-    }
-
-  private:
-    void timer_callback()
-    {
-      auto message = std_msgs::msg::String();
-      message.data = "Hello, world! " + std::to_string(count_++);
-      RCLCPP_INFO(this->get_logger(), "Publishing msg: '%s'", message.data.c_str());
-      publisher_->publish(message);
-    }
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    size_t count_;
-  };
-
-  int main(int argc, char * argv[])
   {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<Publisher>());
-    rclcpp::shutdown();
-    return 0;
+    publisher_ = this->create_publisher<std_msgs::msg::String>("to_stm", 10);
+    timer_ = this->create_wall_timer(1000ms, std::bind(&Publisher::timer_callback, this));
   }
+
+private:
+  void timer_callback()
+  {
+    auto message = std_msgs::msg::String();
+    message.data = "Hello, world! " + std::to_string(count_++);
+    RCLCPP_INFO(this->get_logger(), "Publishing msg: '%s'", message.data.c_str());
+    publisher_->publish(message);
+  }
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  size_t count_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<Publisher>());
+  rclcpp::shutdown();
+  return 0;
+}
--- mros2_echoback/src/sub_node.cpp
+++ mros2_echoback/src/sub_node.cpp
@@ -10,25 +10,25 @@ using std::placeholders::_1;
 
 class Subscriber : public rclcpp::Node
 {
-    public:
-    Subscriber() : Node("mros2_sub")
-    {
-        subscriber_ = this->create_subscription<std_msgs::msg::String>("to_linux", rclcpp::QoS(10).best_effort(), std::bind(&Subscriber::topic_callback, this, _1));
-    }
+public:
+  Subscriber() : Node("mros2_sub")
+  {
+    subscriber_ = this->create_subscription<std_msgs::msg::String>("to_linux", rclcpp::QoS(10).best_effort(), std::bind(&Subscriber::topic_callback, this, _1));
+  }
 
-    private:
-    void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
-    {
-        RCLCPP_INFO(this->get_logger(), "Subscribing msg: '%s'", msg->data.c_str());
-    }
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscriber_;
+private:
+  void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
+  {
+    RCLCPP_INFO(this->get_logger(), "Subscribing msg: '%s'", msg->data.c_str());
+  }
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscriber_;
 };
 
 
 int main(int argc, char *argv[])
 {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<Subscriber>());
-    rclcpp::shutdown();
-    return 0;
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<Subscriber>());
+  rclcpp::shutdown();
+  return 0;
 }
