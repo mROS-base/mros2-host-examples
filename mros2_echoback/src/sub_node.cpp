5d41484a5d17f8f800d70f3930ae29c8c6c70b22
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2021-08-18 18:12:59 +0900

    improve log message

--- mros2_echoback/src/sub_node.cpp
+++ mros2_echoback/src/sub_node.cpp
@@ -19,7 +19,7 @@ public:
 private:
   void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
   {
-    RCLCPP_INFO(this->get_logger(), "Subscribing msg: '%s'", msg->data.c_str());
+    RCLCPP_INFO(this->get_logger(), "Subscribed msg: '%s'", msg->data.c_str());
   }
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscriber_;
 };
