f04bd7f72c68c8954f182e8b2fdf77bbe3c7a9b0
檜原陽一郎 <hibarayoichiro871@gmail.com>
2021-11-21 14:58:43 +0900

    cppType float_t -> float

--- mros2_custom_pubsub/src/custom_pubsub/src/pub_node.cpp
+++ mros2_custom_pubsub/src/custom_pubsub/src/pub_node.cpp
@@ -27,7 +27,7 @@ private:
     auto message = health_msgs::msg::Health();
     message.name = std::to_string(count_++);
     message.height = int8_t 170;
-    message.weight = float_t 63.5;
+    message.weight = float 63.5;
     RCLCPP_INFO(this->get_logger(), "Publishing msg: { name: '%s', height: '%s'cm, weight: '%s'kg }", message.name.c_str(), message.height.c_str(), message.weight.c_str());
     publisher_->publish(message);
   }
