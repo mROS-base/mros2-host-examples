1a466d562b7bc429ce4dc2bb5f2fd6fd8b9c8dd9
檜原陽一郎 <hibarayoichiro871@gmail.com>
2021-12-06 17:27:48 +0900

    mros2_int16/float32_test app added

---
+++ mros2_int16_test/src/sub_node.cpp
@@ -0,0 +1,34 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/int16.hpp"
+
+using std::placeholders::_1;
+
+class Subscriber : public rclcpp::Node
+{
+public:
+  Subscriber() : Node("mros2_sub")
+  {
+    subscriber_ = this->create_subscription<std_msgs::msg::Int16>("to_linux", rclcpp::QoS(10).best_effort(), std::bind(&Subscriber::topic_callback, this, _1));
+  }
+
+private:
+  void topic_callback(const std_msgs::msg::Int16::SharedPtr msg) const
+  {
+    RCLCPP_INFO(this->get_logger(), "Subscribed msg: %d", msg->data);
+  }
+  rclcpp::Subscription<std_msgs::msg::Int16>::SharedPtr subscriber_;
+};
+
+
+int main(int argc, char *argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<Subscriber>());
+  rclcpp::shutdown();
+  return 0;
+}
