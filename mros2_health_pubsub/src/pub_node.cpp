5e493dd46abe2b76da002576f3383321a3e8d41d
檜原陽一郎 <hibarayoichiro871@gmail.com>
2021-12-11 14:41:35 +0900

    pub_node of health_pubsub modified

--- mros2_health_pubsub/src/pub_node.cpp
+++ mros2_health_pubsub/src/pub_node.cpp
@@ -15,7 +15,7 @@ class Publisher : public rclcpp::Node
 {
 public:
   Publisher()
-    : Node("pub_mros2"), count_(999990)
+    : Node("pub_mros2"), count_(1000000)
   {
     publisher_ = this->create_publisher<health_msgs::msg::Health>("to_stm", 10);
     timer_ = this->create_wall_timer(1000ms, std::bind(&Publisher::timer_callback, this));
