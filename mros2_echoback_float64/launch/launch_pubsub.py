550e1cdf48c94ec2ce85b15f0a0d25c072a12677
檜原陽一郎 <hibarayoichiro871@gmail.com>
2021-12-15 20:07:29 +0900

    test f64

--- mros2_echoback_float64/launch/launch_pubsub.py
+++ mros2_echoback_float64/launch/launch_pubsub.py
@@ -5,14 +5,14 @@ from launch_ros.actions import Node
 def generate_launch_description():
   return LaunchDescription([
     Node(
-      package='mros2_echoback_float32',
+      package='mros2_echoback_float64',
       node_executable='pub_node',
       node_name='pub_mros2',
 			prefix=['stdbuf -o L'],
 			output="screen"
     ),
     Node(
-      package='mros2_echoback_float32',
+      package='mros2_echoback_float64',
       node_executable='sub_node',
       node_name='mros2_sub',
 			prefix=['stdbuf -o L'],
