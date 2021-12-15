3900cfd3a78b1b99f53001c54996e238ce426f7f
檜原陽一郎 <hibarayoichiro871@gmail.com>
2021-12-15 18:02:38 +0900

    rename apps and int16->utin16

--- mros2_echoback_float32/launch/launch_pubsub.py
+++ mros2_echoback_float32/launch/launch_pubsub.py
@@ -5,14 +5,14 @@ from launch_ros.actions import Node
 def generate_launch_description():
   return LaunchDescription([
     Node(
-      package='mros2_float32_test',
+      package='mros2_echoback_float32',
       node_executable='pub_node',
       node_name='pub_mros2',
 			prefix=['stdbuf -o L'],
 			output="screen"
     ),
     Node(
-      package='mros2_float32_test',
+      package='mros2_echoback_float32',
       node_executable='sub_node',
       node_name='mros2_sub',
 			prefix=['stdbuf -o L'],
